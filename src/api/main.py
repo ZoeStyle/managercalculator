1b33c776a756561fb85cc986e6984fea2c2d7422
Victor Aparecido Hernandes Santos <regiotimao@gmail.com>
2022-02-17 12:06:37 -0300

    Atualização do readme, main e pipeline
    No readme foi alterado o nome do framework utilizado para uma api rest;
    
    No pipeline foi alterado o titulo de execução;
    
    No main foi adicionado uma nova porta e host para acesso

--- src/api/main.py
+++ src/api/main.py
@@ -3,7 +3,7 @@ from uptime import uptime
 from typing import Any
 from flask import Flask
 from src.domain.handlers.bhaskara_handler import bhaskara_handle
-
+from os import environ
 
 app = Flask(__name__)
 
@@ -22,4 +22,10 @@ async def test_bhaskara(request: dict[str, Any]):
     return bhaskara_handle(request)
 
 if __name__ == "__main__":
-    app.run()
+    try:
+        port = int(environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        port = 5555
+
+    host = environ.get('SERVER_HOST', 'localhost')
+    app.run(host=host, port=port)
