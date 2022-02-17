01ca37038734474429af1a5c95e4f80e9bb0b55d
Victor Aparecido Hernandes Santos <regiotimao@gmail.com>
2022-02-17 12:28:55 -0300

    Atualização das porta e host de acesso

--- src/api/app.py
+++ src/api/app.py
@@ -3,7 +3,6 @@ from uptime import uptime
 from typing import Any
 from flask import Flask
 from src.domain.handlers.bhaskara_handler import bhaskara_handle
-from os import environ
 
 app = Flask(__name__)
 
@@ -22,10 +21,6 @@ async def test_bhaskara(request: dict[str, Any]):
     return bhaskara_handle(request)
 
 if __name__ == "__main__":
-    try:
-        port = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        port = 5555
-
-    host = environ.get('SERVER_HOST', 'localhost')
+    port = 5555
+    host = '0.0.0.0'
     app.run(host=host, port=port)
